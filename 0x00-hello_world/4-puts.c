165fb4f46303b2192c123a8ffd70a546c9fdf000
AlphaUmaruJalloh <aujalloh97@gmail.com>
2022-12-08 16:45:29 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+}
