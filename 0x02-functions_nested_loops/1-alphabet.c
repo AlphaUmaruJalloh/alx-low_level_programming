8b710b11b7a99e9dd986d79891615960b060f7e6
AlphaUmaruJalloh <aujalloh97@gmail.com>
2022-12-13 18:02:18 -0800

    function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
