7c1eb3e60d4259a1dec7ac927a6b0a6ccb8df3f8
AlphaUmaruJalloh <aujalloh97@gmail.com>
2022-12-15 15:39:13 -0800

    function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,6 +18,7 @@ void print_number(int n)
 	{
 		i = n;
 	}
+
 	j = i;
 	count = 1;
 
@@ -26,6 +27,7 @@ void print_number(int n)
 		j /= 10;
 		count *= 10;
 	}
+
 	for (; count >= 1; count /= 10)
 	{
 		_putchar(((i / count) % 10) + 48);
