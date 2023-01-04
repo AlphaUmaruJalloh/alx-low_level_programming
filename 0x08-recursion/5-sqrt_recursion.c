5235e77b6daa18c93cec44c52947dbd32d228c8b
AlphaUmaruJalloh <aujalloh97@gmail.com>
2023-01-04 14:29:28 -0800

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,41 @@
 #include "main.h"
-/**
- * helper - Finds the square root of two numbers
- * @a: The number
- * @b: The number to test for the square root of @a
- *
- * Return: the square root of
- */
 
-int helper(int a, int b)
-{
-	if (b * b > a)
-	{
-		return (-1);
-	}
-	else if (b * b == a)
-	{
-		return (b);
-	}
-	else
-	{
-		return (helper(a, b + 1));
-	}
-	return (1);
-}
+int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt - returns the square root of a number
- * @n: the number to return the square root of
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: the square root of n
+ * Return: the resulting square root
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
+	if (n < 0)
 	{
-		return (0);
+		return (-1);
 	}
-	return (helper(n, 1));
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
 }
